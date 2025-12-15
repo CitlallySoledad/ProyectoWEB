35bc2d3516b23528e460f63b9790dd438c864585
CitlallySoledad <xitla134@gmail.com>
2025-12-15 16:37:45 -0600

    Arreglos 4

---
+++ resources/lang/en/validation.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'password' => [
+        'min'     => 'La nueva contraseña debe tener al menos 8 caracteres.',
+        'mixed'   => 'La nueva contraseña debe incluir al menos una mayúscula y una minúscula.',
+        'numbers' => 'La nueva contraseña debe incluir al menos un número.',
+        'symbols' => 'La nueva contraseña debe incluir al menos un símbolo.',
+    ],
+];
